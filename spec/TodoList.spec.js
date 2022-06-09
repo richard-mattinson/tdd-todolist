8be5f4bf33820bda324c1d7835f44c7850ac0817
Richard Mattinson <richard.m@ttinson.net>
2022-06-09 14:33:44 +0100

    started

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -1,17 +1,68 @@
-const TodoList = require("../src/TodoList.js")
+/* For each requirement below, write a single test and pass it by writing source code. Repeat until you have implemented all requirements. */
 
-describe("TodoList", () => {
-  it("creates a todo item", () => {
+/*
+- Create a todo item that has an ID, text description, and starts off incomplete
+- Get all todo items
+- Set a todo completed by its ID
+- Get only todo items that are incomplete
+- Get only todo items that are complete
+- Search and return a todo item by its ID, or return a message saying it doesn’t exist
+- Remove a todo item by its ID
+*/
+
+const TodoList = require('../src/TodoList.js')
+
+describe('TodoList', () => {
+  it('creates a todo item', () => {
     // set up
     const todoList = new TodoList()
     const expected = {
       id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
+      text: 'Fill up the cold brew jug!',
+      completed: false
     }
     // execute
-    const result = todoList.create("turn the heating on!")
+    const result = todoList.create('Fill up the cold brew jug!')
     // verify
     expect(result).toEqual(expected)
+  })
+
+  it('get all tasks from the todo list', () => {
+    // set up
+    const todoList = new TodoList()
+    const first = {
+      id: 1,
+      text: 'Fill up the cold brew jug!',
+      completed: false
+    }
+
+    const second = {
+      id: 2,
+      text: 'Make sense of JavaScript!',
+      completed: false
+    }
+
+    const third = {
+      id: 3,
+      text: 'Go 2-0 on Kombat League!',
+      completed: false
+    }
+    // execute
+    todoList.create(first.text)
+    const result = todoList.create(first.text, second.text, third.text)
+    // verify
+    expect(result).toEqual(first, second, third)
+  })
+
+  it('set a task as completed', () => {
+    const todoList = new TodoList()
+    const first = {
+      id: 1,
+      text: 'Fill up the cold brew jug!',
+      completed: true
+    }
+    todoList.create(first.text)
+    const result = todoList.taskCompleted(first.id)
+    expect(result).toEqual(first)
   })
 })
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,35 @@
 class TodoList {
+  constructor() {
+    this.id = 1
+    this.task = []
+  }
 
+  // Create method
+  create(taskName) {
+    const newTask = {
+      id: this.id,
+      text: taskName,
+      completed: false
+    }
+    this.id++
+
+    this.task.push(newTask)
+    return newTask
+  }
+
+  taskCompleted(id) {
+    let locatedTask = false
+    for (let i = 0; i < this.task.length; i++) {
+      let currentTask = this.task[i]
+      if (currentTask.id === id) {
+        locatedTask = currentTask
+      }
+    }
+    if (locatedTask !== false) {
+      locatedTask.completed = true
+    }
+    return locatedTask
+  }
 }
 
 module.exports = TodoList
